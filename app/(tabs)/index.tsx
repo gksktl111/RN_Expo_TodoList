7177aec83397a5e28d563b6ae6cb9c07747c2ad9
pointtt <50075024+gksktl111@users.noreply.github.com>
2024-07-03 16:09:02 +0900

    feat: todo 리스트 삽입, 삭제, 스크롤 구현

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,90 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { Animated, Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from "react";
+import ScrollView = Animated.ScrollView;
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+export default function App() {
+    const [enteredGoalsText, setEnteredGoalsText] = useState<string>('');
+    const [courseGoals, setCourseGoals] = useState<string[]>([]);
 
-export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
-  );
+    const goalInputHandler = (enteredText: string) => {
+        setEnteredGoalsText(enteredText);
+    }
+
+    const addGoalHandler = () => {
+        if (enteredGoalsText === '') return;
+        setCourseGoals(currentCourseGoals => [...currentCourseGoals, enteredGoalsText]);
+        setEnteredGoalsText(''); // TextInput 필드를 초기화
+    }
+
+    const deleteGoalHandler = (index: number) => {
+        setCourseGoals(currentCourseGoals =>
+            currentCourseGoals.filter((goal, goalIndex) => goalIndex !== index)
+        );
+    }
+
+    const renderItem = ({ item, index }: { item: string, index: number }) => (
+        <View style={styles.goalsItem}>
+            <Text>{item}</Text>
+            <Button title="삭제버튼" onPress={() => deleteGoalHandler(index)} />
+        </View>
+    );
+
+    return (
+        <View style={styles.container}>
+            <StatusBar style={"dark"} />
+            <View style={styles.inputContainer}>
+                <TextInput
+                    style={styles.textInput}
+                    placeholder="목표를 적어주세요!"
+                    onChangeText={goalInputHandler}
+                    value={enteredGoalsText}
+                />
+                <Button title="목표 추가하기" onPress={addGoalHandler} />
+            </View>
+            <FlatList
+                data={courseGoals}
+                renderItem={renderItem}
+                // keyExtractor 를 사용해 FlatList 가 키를 인식할 수 있게함
+                keyExtractor={(item, index) => index.toString()} // keyExtractor 추가
+                contentContainerStyle={styles.goalsContainer}
+            />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+    container: {
+        flex: 1,
+        paddingTop: 50,
+        paddingHorizontal: 16,
+        backgroundColor: 'white',
+    },
+    inputContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: 'center',
+        borderBottomWidth: 1,
+        borderBottomColor: '#cccccc',
+        paddingBottom:20,
+        marginBottom: 20,
+    },
+    textInput: {
+        borderWidth: 1,
+        borderColor: '#cccccc',
+        width: '70%',
+        paddingLeft: 8,
+    },
+    goalsContainer: {
+        flexGrow: 1,
+    },
+    goalsItem: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 8,
+        marginBottom: 16,
+        borderColor: 'gray',
+        borderBottomWidth: 1,
+    }
 });
